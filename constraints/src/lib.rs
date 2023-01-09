59123b524aadd8b36889f4ac71258ce207212aac
Vincent Esche <regexident@gmail.com>
2023-01-09 13:55:12 +0100

    Add `#![warn(missing_docs)]` for all crates

--- constraints/src/lib.rs
+++ constraints/src/lib.rs
@@ -1,6 +1,7 @@
 //! Pure Rust implementation of the constraint logic defined in the ["Media Capture and Streams"][mediacapture_streams] spec.
 //!
 //! [mediacapture_streams]: https://www.w3.org/TR/mediacapture-streams/
+#![warn(missing_docs)]
 
 pub mod algorithms;
 pub mod errors;
--- media/src/lib.rs
+++ media/src/lib.rs
@@ -1,4 +1,5 @@
 #![warn(rust_2018_idioms)]
+#![warn(missing_docs)]
 #![allow(dead_code)]
 
 pub mod audio;
